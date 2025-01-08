c1de307f5799e2d476e25d7414be30f3a6ed1707
Michael Little <michael.little@xero.com>
2025-01-08 22:46:31 +1300

    Very very basic http socket application

--- WebServerTutorial/Program.cs
+++ WebServerTutorial/Program.cs
@@ -1,2 +1,47 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var ipEndPoint = new IPEndPoint(IPAddress.Any, 9000);
+using var serverSocket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+serverSocket.Bind(ipEndPoint);
+serverSocket.Listen();
+
+while (true)
+{
+    using var connectionSocket = serverSocket.Accept();
+    Console.WriteLine("Connection accepted");
+
+    var buffer = new ArraySegment<byte>(new byte[2048]);
+
+    var sb = new StringBuilder();
+
+    var requestHasMoreBytes = true;
+
+    while (requestHasMoreBytes)
+    {
+        var bytesReceived = await connectionSocket.ReceiveAsync(buffer);
+        sb.Append(Encoding.UTF8.GetString(buffer[new Range(0, bytesReceived - 1)]));
+
+
+        requestHasMoreBytes = bytesReceived == 2048;
+    }
+
+    Console.WriteLine("Received request:");
+    Console.WriteLine(sb.ToString());
+    Console.WriteLine("------");
+
+
+    var responseBytes = """
+                        HTTP/1.0 200 OK
+                        Content-Type: text/plain
+
+                        Thank you!
+                        """u8.ToArray();
+
+    connectionSocket.Send(responseBytes);
+
+}
