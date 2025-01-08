6b2f4247496f1646345c3bb0acc22e24279bc4f2
Michael Little <michael.little@xero.com>
2025-01-08 22:54:33 +1300

    Echo server

--- WebServerTutorial/Program.cs
+++ WebServerTutorial/Program.cs
@@ -1,8 +1,6 @@
-﻿
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
 
 var ipEndPoint = new IPEndPoint(IPAddress.Any, 9000);
 using var serverSocket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
@@ -13,35 +11,29 @@ serverSocket.Listen();
 while (true)
 {
     using var connectionSocket = serverSocket.Accept();
-    Console.WriteLine("Connection accepted");
 
     var buffer = new ArraySegment<byte>(new byte[2048]);
 
-    var sb = new StringBuilder();
+    var requestStringBuilder = new StringBuilder();
 
     var requestHasMoreBytes = true;
 
     while (requestHasMoreBytes)
     {
         var bytesReceived = await connectionSocket.ReceiveAsync(buffer);
-        sb.Append(Encoding.UTF8.GetString(buffer[new Range(0, bytesReceived - 1)]));
-
+        requestStringBuilder.Append(Encoding.UTF8.GetString(buffer[new Range(0, bytesReceived - 1)]));
 
         requestHasMoreBytes = bytesReceived == 2048;
     }
 
-    Console.WriteLine("Received request:");
-    Console.WriteLine(sb.ToString());
-    Console.WriteLine("------");
+    var responseBytes = Encoding.UTF8.GetBytes($"""
+                                                HTTP/1.0 200 OK
+                                                Content-Type: text/plain
 
+                                                Received request:
 
-    var responseBytes = """
-                        HTTP/1.0 200 OK
-                        Content-Type: text/plain
-
-                        Thank you!
-                        """u8.ToArray();
+                                                {requestStringBuilder}
+                                                """);
 
     connectionSocket.Send(responseBytes);
-
 }
