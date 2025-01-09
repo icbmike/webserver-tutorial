10dac7ee2e549ea4584f8c057fa845fd3e6597fd
Michael Little <michael.little@xero.com>
2025-01-10 10:25:38 +1300

    Basic reflection based routing

---
+++ WebServerTutorial/Controller.cs
@@ -0,0 +1,24 @@
+﻿namespace WebServerTutorial;
+
+public class Controller
+{
+    public HttpResponse GetHello(HttpRequest request)
+    {
+        return new HttpResponse(
+            200,
+            "OK",
+            new Dictionary<string, string> { { "Content-Type", "text/plain" } },
+            $"Hello"
+        );
+    }
+
+    public HttpResponse GetGoodbye(HttpRequest request)
+    {
+        return new HttpResponse(
+            200,
+            "OK",
+            new Dictionary<string, string> { { "Content-Type", "text/plain" } },
+            $"Goodbye"
+        );
+    }
+}
--- WebServerTutorial/RequestHandler.cs
+++ WebServerTutorial/RequestHandler.cs
@@ -4,13 +4,25 @@ public class RequestHandler
 {
     public static HttpResponse HandleRequest(HttpRequest request)
     {
-        var (method, path, host, headers) = request;
+        var (method, path, _, _) = request;
+
+        var controller = new Controller();
+
+        var methodInfo = controller.GetType().GetMethods()
+            .SingleOrDefault(info =>
+                info.Name.StartsWith(method, StringComparison.InvariantCultureIgnoreCase) &&
+                string.Equals(info.Name.Substring(method.Length), path.Substring(1), StringComparison.InvariantCultureIgnoreCase));
+
+        if (methodInfo != null)
+        {
+            return (HttpResponse)methodInfo.Invoke(controller, [request])!;
+        }
 
         return new HttpResponse(
-            200,
-            "OK",
+            404,
+            "Not Found",
             new Dictionary<string, string> { { "Content-Type", "text/plain" } },
-            $"Request received: {method} {path}"
+            $"Not Found"
         );
     }
 }
